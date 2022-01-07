4d0c3eeac9f4a8a58b0767e1c33b805dce992ceb
yurimarim <yurimarim@hotmail.com>
2022-01-07 20:59:07 -0300

    Updating assets and Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { ScrollView, ActivityIndicator } from 'react-native'
+import { View, ScrollView, ActivityIndicator } from 'react-native'
 
 import { Feather } from '@expo/vector-icons'
 import {
